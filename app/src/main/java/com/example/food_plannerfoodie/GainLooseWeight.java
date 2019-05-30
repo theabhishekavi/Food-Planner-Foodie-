7731c115872269d9f8123f57d5c4100ce7e7a44c
theabhishekavi <abhishekavi4602@gmail.com>
2019-05-30 10:51:20 +0530

    Navigation view ui

---
+++ app/src/main/java/com/example/food_plannerfoodie/GainLooseWeight.java
@@ -0,0 +1,6 @@
+package com.example.food_plannerfoodie;
+
+import android.support.v4.app.Fragment;
+
+public class GainLooseWeight extends Fragment {
+}
---
+++ app/src/main/java/com/example/food_plannerfoodie/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.food_plannerfoodie;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.nav_view);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener
+                (new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                drawerLayout.closeDrawers();
+                menuItem.setChecked(true);
+                return true;
+            }
+        });
+    }
+}
