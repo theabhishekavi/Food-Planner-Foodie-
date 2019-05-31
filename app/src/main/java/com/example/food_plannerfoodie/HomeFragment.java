83481018ca59c41b1f4c8bbe6a36361f64dcfeb2
theabhishekavi <abhishekavi4602@gmail.com>
2019-05-31 17:58:04 +0530

    implements View.Onclicklistener interface

--- app/src/main/java/com/example/food_plannerfoodie/HomeFragment.java
+++ app/src/main/java/com/example/food_plannerfoodie/HomeFragment.java
@@ -10,7 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -26,46 +26,38 @@ public class HomeFragment extends Fragment {
         CardView cardView_wym = view.findViewById(R.id.cardview_whats_your_motive);
         CardView cardView_dfi = view.findViewById(R.id.cardview_daily_food_intake);
 
-        cardView_bfp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager fragmentManager = getFragmentManager();
-                fragmentManager.beginTransaction().replace(R.id.content_frame,
-                        new BFPFragment()).addToBackStack(null).commit();
-            }
-        });
-        cardView_bmi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager fragmentManager = getFragmentManager();
-                fragmentManager.beginTransaction().replace(R.id.content_frame,
-                        new BMIFragment()).addToBackStack(null).commit();
-            }
-        });
-        cardView_bmr.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager fragmentManager = getFragmentManager();
-                fragmentManager.beginTransaction().replace(R.id.content_frame,
-                        new BMRFragment()).addToBackStack(null).commit();
-            }
-        });
-        cardView_wym.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager fragmentManager = getFragmentManager();
-                fragmentManager.beginTransaction().replace(R.id.content_frame,
-                        new WhatsYourMotiveFragment()).addToBackStack(null).commit();
-            }
-        });
-        cardView_dfi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager fragmentManager = getFragmentManager();
-                fragmentManager.beginTransaction().replace(R.id.content_frame,
-                        new DailyFoodIntakeFragment()).addToBackStack(null).commit();
-            }
-        });
+        cardView_bfp.setOnClickListener(this);
+        cardView_bmi.setOnClickListener(this);
+        cardView_bmr.setOnClickListener(this);
+        cardView_wym.setOnClickListener(this);
+        cardView_dfi.setOnClickListener(this);
+
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onClick(View v) {
+        FragmentManager fragmentManager = getFragmentManager();
+        Fragment fragment = new HomeFragment();
+        switch (v.getId()){
+
+            case R.id.cardview_bmi:
+                fragment = new BMIFragment();
+                break;
+            case R.id.cardview_bmr:
+                fragment = new BMRFragment();
+                break;
+            case R.id.cardview_bfp:
+                fragment = new BFPFragment();
+                break;
+            case R.id.cardview_daily_food_intake:
+                fragment = new DailyFoodIntakeFragment();
+                break;
+            case R.id.cardview_whats_your_motive:
+                fragment = new WhatsYourMotiveFragment();
+                break;
+        }
+        fragmentManager.beginTransaction().replace(R.id.content_frame,
+                fragment).addToBackStack(null).commit();
     }
 }
